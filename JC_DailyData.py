87a2b50927e6f2edf57029a03e61ed3dc6d35afb
stella0320 <jud40322@gmail.com>
2023-10-17 17:56:15 +0800

    update JC_DailyData.py API

--- JC_DailyData.py
+++ JC_DailyData.py
@@ -1,6 +1,6 @@
 from flask import *
 from flask import Blueprint
-
+import requests
 import os
 from dotenv import load_dotenv
 load_dotenv()
@@ -9,7 +9,18 @@ API_key = os.getenv("Alice_API_key")
 DailyData_api = Blueprint("dailyData", __name__, static_folder="static", template_folder="templates")
 
 data_key = API_key
-data_url = "https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-D0047-091?Authorization="
+data_url = "https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization="
+
+def getDailyData(location_name):
+    response = requests.get(data_url+data_key+"&locationName="+location_name)
+    data = response.json()
+    return data['records']['location'][0]
+
+@DailyData_api.route("/")
+def get_daily_data():
+    location_name = str(request.args.get("locationName", ""))
+    result = getDailyData(location_name)
+    return jsonify(result)
 
 @DailyData_api.route("/test")
 def test():
